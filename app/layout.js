43db076faf6553a63f9c42ca505ba29a3888b7ed
Shailesh Chaudhari <shailesh93602@gmail.com>
2024-11-16 14:22:56 +0530

    Added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} ${firaCode.variable}`}>
       <body className="bg-background text-text-primary font-sans min-h-screen flex flex-col">
+        <Analytics />
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-8">
           {children}
